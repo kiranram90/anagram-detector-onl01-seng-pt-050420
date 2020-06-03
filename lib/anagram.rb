c10bd4f4e9b9d14749b14c3383837f512702cc18
kiran ram <kram@lions.molloy.edu>
2020-06-03 01:53:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  @@word = []
+  
+  def initialize(word)
+    @@word << self
+  end
+  
+  def match(word)
+   new_word = [word.split("")]
+    new_word.sort == @@word.sort
+  end
+  
+end
